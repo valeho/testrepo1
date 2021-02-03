4781918acaee8bced03de3fc66d1f38584842c2e
valeho <e.rezonov@yandex.ru>
2021-02-03 20:17:06 +0300

    Text

--- ssh_sync.bat
+++ ssh_sync.bat
@@ -1,6 +1,7 @@
-git clone https://github.com/valeho/testrepo1.git
+%Answer% := git clone https://github.com/valeho/testrepo1.git
+echo "Answer" + Answer
 git remote add https://github.com/valeho/testrepo2.git
 git fetch
 git checkout main
 git merge main
-git push origin
+git push origin123
